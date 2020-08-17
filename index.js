e826d2f31248e423830ea822367b3f4a9893f549
Ville <vilunen@gmail.com>
2020-08-17 16:42:06 +0300

    Added cors and Procfile

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function(tokens, req, res) {
     return [
@@ -91,7 +93,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
