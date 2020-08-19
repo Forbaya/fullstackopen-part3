2a5deca4547af60a5e3dd5540c3d0fd85cd0ee8e
Ville <vilunen@gmail.com>
2020-08-19 12:38:23 +0300

    Part 3: 3.17*: Phonebook database, step5

--- index.js
+++ index.js
@@ -74,6 +74,21 @@ app.post('/api/persons', (req, res) => {
     })
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson.toJSON())
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndpoint = (req, res) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
