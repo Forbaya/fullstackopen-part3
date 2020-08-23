0c751bb660621e6f9134968f34f1225d7d8cb84e
Ville <vilunen@gmail.com>
2020-08-23 13:01:25 +0300

    3.21: Deploying the database backend to production

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.put('/api/persons/:id', (req, res, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true })
+    Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             res.json(updatedPerson.toJSON())
         })
